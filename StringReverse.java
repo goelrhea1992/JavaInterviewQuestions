e1aac64139cc812a35f4872776d3e8f188fbd50d
Rhea Goel <goelrhea1992@gmail.com>
2015-07-17 18:30:20 -0700

    Update StringReverse.java

--- StringReverse.java
+++ StringReverse.java
@@ -1,15 +1,14 @@
 
 public class StringReverse {
-public static void main(String args[]) {
-	String var = "arjun";
-	char[] tempvar = var.toCharArray();
-	  for (int i = 0; i< tempvar.length/2; i++) {
-		  char temp = tempvar[i];
-		  tempvar[i] = tempvar[var.length() - i -1];
-		  tempvar[tempvar.length - i -1] = temp;
-	  }
-	  
-	  var = new String(tempvar);
-	  System.out.println(var);
-}
+	public static void main(String args[]) {
+		String var = "arjun";
+		char[] tempvar = var.toCharArray();
+		  for (int i = 0; i< tempvar.length/2; i++) {
+			  char temp = tempvar[i];
+			  tempvar[i] = tempvar[var.length() - i -1];
+			  tempvar[tempvar.length - i -1] = temp;
+		  }
+		  var = new String(tempvar);
+		  System.out.println(var);
+	}
 }
