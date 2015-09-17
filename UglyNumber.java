b5beb102a694192b315fdce58bfd5ed710e6ee31
Rhea Goel <goelrhea1992@gmail.com>
2015-09-17 17:24:56 -0400

    Create UglyNumber.java

---
+++ UglyNumber.java
@@ -0,0 +1,24 @@
+/* Problem from Leetcode.
+* Write a program to check whether a given number is an ugly number.
+* Ugly numbers are positive numbers whose prime factors only include 2, 3, 5. 
+* For example, 6, 8 are ugly while 14 is not ugly since it includes another prime factor 7.
+* Note that 1 is typically treated as an ugly number.
+*/
+
+public class Solution {
+    public boolean isUgly(int num) {
+        if (num == 1) 
+            return true;
+        if (num <= 0)
+            return false;
+        List<Integer> desiredPrimes = Arrays.asList(2, 3, 5);
+        for (int prime : desiredPrimes) {
+            while (num % prime == 0) {
+                num = num / prime;
+            }
+        }
+        if (num == 1)
+            return true;
+        return false;
+    }
+}
