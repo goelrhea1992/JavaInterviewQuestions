609b7c6943bd1738e9511864694d91257991aea2
Rhea Goel <goelrhea1992@gmail.com>
2016-07-25 21:13:51 -0700

    Update MagicBox.java

--- MagicBox.java
+++ MagicBox.java
@@ -1,6 +1,6 @@
 /**
  * Consider a 2 D array of 0s and 1s.
- * Find the maximum number of rows that can be made all ones (each eleemnt in the row is a '1'), 
+ * Find the maximum number of rows that can be made all ones (each element in the row is a '1'), 
  * with minimum number of column flips.
  * Note: You're only allowed to flip entire columns, and not individual elements. 
  * You can flip as many times as you like - though flipping twice gives you back the same row :)
@@ -17,7 +17,7 @@ public class MagixBox {
 		
 		HashMap<String, Integer> rowFrequency = new HashMap<String, Integer>();
 		int i, nZeros = 0;
-		int nTestCases, nRows, nCols, j=0;
+		int nTestCases, nRows, nCols, j = 0;
 		int[][] arr;
 		int row,col;
 		String rowString = "";
@@ -27,7 +27,7 @@ public class MagixBox {
 
 		Scanner in = new Scanner(System.in);
 		nTestCases = in.nextInt();
-		for(j=0; j<nTestCases; j++) {
+		for (j = 0; j < nTestCases; j++) {
 			
 			rowFrequency = new HashMap<String, Integer>();
 			nZeros = 0;
@@ -36,34 +36,32 @@ public class MagixBox {
 			nRows = in.nextInt();
 			nCols = in.nextInt();
 			
-			if(nCols==1 || nRows==1) {
+			if (nCols == 1 || nRows == 1) {
 				System.out.println(nRows);
 				continue;
 			}
 				
 			arr = new int[nRows][nCols];
 			
-			for(row=0; row<nRows; row++) {
-				for(col=0; col<nCols; col++) {
+			for (row = 0; row < nRows; row++) {
+				for (col = 0; col < nCols; col++) {
 					arr[row][col] = in.nextInt();
 				}
 			}
 	
-			for(row=0; row<nRows; row++) {
+			for (row = 0; row < nRows; row++) {
 				rowString = "";
-				for(col=0; col<nCols; col++) {
+				for (col = 0; col < nCols; col++) {
 					rowString = rowString + arr[row][col];
 				}
 				rowComplementString = complementString(rowString);
-				if(rowFrequency.containsKey(rowString)) {
+				if (rowFrequency.containsKey(rowString)) {
 					thisFrequency = rowFrequency.get(rowString);
 					rowFrequency.put(rowString, thisFrequency + 1);
-				}
-				else if (rowFrequency.containsKey(rowComplementString)) {
+				} else if (rowFrequency.containsKey(rowComplementString)) {
 					thisFrequency = rowFrequency.get(rowComplementString);
 					rowFrequency.put(rowComplementString, thisFrequency + 1);
-				}
-				else {
+				} else {
 					rowFrequency.put(rowString, 1);
 				}
 			}
@@ -83,9 +81,9 @@ public class MagixBox {
 		String complementString = "";
 		int i;
 		int thisBit;
-		for(i=0; i<rowString.length(); i++) {
-			thisBit = Integer.parseInt(rowString.substring(i,i+1));
-			complementString = complementString + ((thisBit>0)?0:1);
+		for (i = 0; i < rowString.length(); i++) {
+			thisBit = Integer.parseInt(rowString.substring(i, i + 1));
+			complementString = complementString + ((thisBit > 0) ? 0 : 1);
 		}
 		return complementString;
 	}
